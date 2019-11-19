0beea41d8d6d8040452840869a43b79915116d22
Elise Guezel <elise.guezel@yahoo.fr>
2019-11-19 17:21:01 +0100

    [ADD] Create basic apparence for FormTodo component

--- src/Components/FormTodo.jsx
+++ src/Components/FormTodo.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 const FormTodo = () => {
   return(
-    <div>FormTodo</div>
+    <div className="card mb-3">
+      <div className="card-body">
+        <div class="form-group">
+          <label for="Title">Title</label>
+          <input type="text" class="form-control" id="Title" placeholder="Title"/>
+        </div>
+        <div class="form-group">
+          <label for="Description">Description</label>
+          <input type="text" class="form-control" id="Description" placeholder="Description"/>
+        </div>
+        <button className="btn btn-primary">Add</button>
+      </div>
+    </div>
   )
 }
 
